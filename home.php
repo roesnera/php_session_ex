516107fc66fbb73b4e5323faa40c2af03849a965
roesnera <adam.roesner@generation.org>
2023-06-27 20:13:18 -0400

    completed login form

--- home.php
+++ home.php
@@ -1,25 +1,31 @@
 <?php
+// We need to use sessions, so you should always start sessions using the below code.
 session_start();
-$username = $_SESSION["username"];
-$password = $_SESSION["password"];
-echo "Username: {$username}<br>Password: {$password}";
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit;
+}
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="home.php" method="post">
-        <input type="submit" name="logout" value="logout">
-    </form>
-</body>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Home Page</title>
+		<link href="styles.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Website Title</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Home Page</h2>
+			<p>Welcome back, <?=$_SESSION['name']?>!</p>
+		</div>
+	</body>
 </html>
-<?php
-    if(isset($_POST["logout"])){
-        session_destroy();
-        header("Location: index.php");
-    }
-?>
